cab39a179940d9d1f2a50c1fd32255b90fd1007f
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-20 23:15:58 +0200

    init

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+cd $(dirname $0)
+
+NODE_TAG=v24.14.1
+
+git clone --depth 1 --branch "$NODE_TAG" https://github.com/nodejs/node.git node-src
+cd node-src
+git apply ../patches/support_v8_sandboxed_pointers.patch
+./configure \
+    --ninja \
+    --experimental-enable-pointer-compression \
+    --experimental-pointer-compression-shared-cage \
+    --without-siphash \
+    --prefix=/usr/local/workspace/node-release
+make
