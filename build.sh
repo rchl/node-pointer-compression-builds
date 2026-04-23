d2509336417aec76096819072b9923e1d79ddbe3
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-23 14:19:44 +0200

    shorter release title

--- build.sh
+++ build.sh
@@ -8,13 +8,14 @@ NODE_DIR="node-src-$NODE_TAG"
 
 if [ ! -d "$NODE_DIR" ]; then
   git clone --depth 1 --branch "$NODE_TAG" https://github.com/nodejs/node.git "$NODE_DIR"
-  cd "$NODE_DIR" && ./../script/git-import-patches ../patches
+  cd "$NODE_DIR" && ./../script/git-import-patches ../patches && cd -
 fi
 cd "$NODE_DIR"
 ./configure \
     --ninja \
     --experimental-enable-pointer-compression \
     --experimental-pointer-compression-shared-cage \
+    --openssl-use-def-ca-store \
     --without-siphash \
     --prefix=/usr/local
 make
